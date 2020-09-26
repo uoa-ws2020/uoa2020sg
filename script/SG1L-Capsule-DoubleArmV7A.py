16904c7b61f41cc505afac25bf4806381aa0bf2f
Fumiaki Abe <71297611+AbeFumiaki42@users.noreply.github.com>
2020-09-26 15:58:32 +0900

    Add files via upload

---
+++ script/SG1L-Capsule-DoubleArmV7A.py
@@ -0,0 +1,3 @@
+import WRSUtil
+WRSUtil.loadProject(
+    "MultiSceneViews", "SG1L-Capsule", "AGXSimulator", "DoubleArmV7A")
---
+++ script/SG1M-Capsule-Quadcopter.py
@@ -0,0 +1,4 @@
+import WRSUtil
+WRSUtil.loadProject(
+    "MultiSceneViews", "SG1M-Capsule", [ "AGXSimulator", "AISTSimulator" ], "Quadcopter",
+    enableMulticopterSimulation = True)
